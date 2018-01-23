fab9abf8a8adf33e2c407451f305f3a173b5d1f8
Sneha Sinha <sneha16098@iiitd.ac.in>
2018-01-23 17:46:19 +0530

    exit, pwd, cd, history, echo ---------- ls, cat

--- cat.c
+++ cat.c
@@ -1,9 +1,36 @@
 //cat
 #include <stdio.h>
+#include <stdlib.h>
 int main(int argc, char **argv){
-  printf("%s\n","hey");
+  //printf("%s\n","hey");
   //printf("%s\n", argv[1]);
+  //SetHomeDirectory();
+  char gg[3000];
+  char *xx=getcwd(gg, sizeof(gg));
+  chdir(xx);
+  // int st = strcmp("writeup.txt",argv[1]);
+  // printf("%d",st);
+  char *ff =argv[1];
+  ff[strlen(ff)-1]='\0';
+  //int st = strcmp("writeup.txt",ff);
+  //printf("%d",st);
+  FILE *file = fopen(ff,"r");
+  char *tok;
+  if(file!=NULL){
+    tok = fgetc(file);
+    while (tok != EOF)
+    {
+        printf ("%c", tok);
+        tok = fgetc(file);
+    }
 
+
+    //printf("%s",c);
+  }
+  else{
+    printf("%s","File is not present in the directory.");
+  }
+  fclose(file);
 
   return 1;
 }
